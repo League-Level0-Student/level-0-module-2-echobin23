d1aeb88fa20f5b9f544970d07aa93bb7011a3c4b
themasterofiron <owendouglas0528>
2017-11-25 11:23:57 -0800

    animalfarm,rockpaperscissors

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -18,7 +18,9 @@ AnimalFarm() {
 	 * 1. Ask the user which animal they want, then play the sound of that
 	 * animal.
 	 */
+  JOptionPane.showInputDialog("Which animal do you want");
 
+  
 	/* 2. Make it so that the user can keep entering new animals. */
 }
 
